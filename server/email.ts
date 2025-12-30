1cd34fae7b691c00eda94150dc3646365bf80664
shxdowmouse-debug <shxdowmouse@gmail.com>
2025-12-30 15:19:32 +0000

    Create email.ts

---
+++ server/email.ts
@@ -0,0 +1,20 @@
+import nodemailer from "nodemailer";
+
+export async function sendEmail(to: string, subject: string, html: string) {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS
+    }
+  });
+
+  await transporter.sendMail({
+    from: process.env.GMAIL_USER,
+    to,
+    subject,
+    html
+  });
+}
